f3ce231099d573e58ecba4ecc82f637bf1a84d78
Val <129915613+ValeriaQuinttero@users.noreply.github.com>
2026-06-09 10:16:07 -0600

    Update server.js

--- Nube/server.js
+++ Nube/server.js
@@ -28,4 +28,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Servidor en http://localhost:${PORT}`));
